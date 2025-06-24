34ebf603196bf5f57f41cc73e68f2271bcf2fe51
Hans <gohhansheng@outlook.com>
2025-06-24 12:54:37 +0800

    init project, done with flask api call and html, todo: pass show data to chart, somehow make chart visualise everything, make ui prettier

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# cs50_finalproject
+## Game Plan
+#### app.py
+- retrieve search box data
+- function to return dict of show_episode: show_rating
+- connect data to chart-setup.js
+
+#### index.html
+- create search box with variable to pass to app.py
+
+#### chart-setup.js
+- retrieve data from app.py
---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        show_name = request.form.get("show_name")
+        if show_name:
+            episode_list = get_chart_data(show_name)
+            name_list = [episode.name for episode in episode_list]
+            rating_list = [episode.rating for episode in episode_list]
+            episode_number_list = [episode.episode_number for episode in episode_list]
+            return render_template("index.html", name_list=name_list, rating_list=rating_list, episode_number_list=episode_number_list)
+    return render_template("index.html")
+
+# getting data from the api
+apikey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNDQ4YjdiYTAxODE3MDZmMjUzOTM4MmU4MWIwMDI4MyIsIm5iZiI6MTc0OTcxMTgyOC45NTEsInN1YiI6IjY4NGE3YmQ0MjkzZTk0Njc5NDlmNWRkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.49hBLiPFw1WWg5-4glrqAvMYHeOqexHFZt2Vk8A3YXk"
+headers = {
+    "Authorization": f"Bearer {apikey}",
+}
+
+class Episode:
+    def __init__(self, name, episode_number, rating):
+        self.name = name
+        self.episode_number = episode_number # format: "1x1"
+        self.rating = rating
+
+def get_chart_data(show_name):
+    show_id = get_show_id(show_name)
+    no_seasons = get_no_seasons(show_id)
+    episode_list = []
+    for season in no_seasons:
+        episodes = get_episodes(show_id, season)
+        for episode in episodes:
+            episode_name = episode["name"]
+            episode_number = episode["episode_number"]
+            rating = episode["vote_average"]
+            episode = Episode(episode_name, f"{season}x{episode_number}", rating) # format: "1x1"
+            episode_list.append(episode)
+    return episode_list
+
+def get_show_id(name):
+    params = {
+        "query": name
+    }
+    url = f"https://api.themoviedb.org/3/search/tv"
+    rawdata = requests.get(url, headers=headers, params=params)
+    newdata = rawdata.json()
+    return newdata["results"][0]["id"]
+
+def get_no_seasons(id):
+    url = f"https://api.themoviedb.org/3/tv/{id}"
+    rawdata = requests.get(url, headers=headers)
+    newdata = rawdata.json()
+    seasonlist = []
+    for season in newdata["seasons"]:
+        seasonlist.append(season["season_number"])
+    return seasonlist
+
+def get_episodes(id, season_number):
+    url = f"https://api.themoviedb.org/3/tv/{id}/season/{season_number}"
+    rawdata = requests.get(url, headers=headers)
+    newdata = rawdata.json()
+    episode_list = []
+    for episode in newdata["episodes"]:
+        episode_list.append(episode["episode_number"])
+    return episode_list
+
+if __name__ == "__main__":
+    app.run(debug=True)
